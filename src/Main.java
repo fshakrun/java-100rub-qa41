29d384cbf525312c9aea0ed556e32696fe3bf969
Fedor Shakrun <fshakrun@gmail.com>
2022-07-13 12:33:45 +0300

    camel case and code reformating

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initial_balance = 2000;
-        int top_up = 1100;
+        int initialBalance = 2000;
+        int topUp = 1100;
 
         int bonuses;
 
-            if (top_up > 1000)
-            {bonuses = top_up*1/100;}
-            else
-                bonuses = 0;
+        if (topUp > 1000) {
+            bonuses = topUp * 1 / 100;
+        } else
+            bonuses = 0;
 
-            int final_balance = initial_balance + top_up + bonuses;
+        int finalBalance = initialBalance + topUp + bonuses;
 
-            System.out.println("Итоговый баланс равен " + final_balance);
-            System.out.println("Начислено бонусных рублей "+ bonuses);
+        System.out.println("Итоговый баланс равен " + finalBalance);
+        System.out.println("Начислено бонусных рублей " + bonuses);
 
     }
 }
